af4456d174d4378de43cf500bef49dcfca5332ae
ew32 <ew32@naver.com>
2023-04-08 19:32:10 +0900

    .

---
+++ index.js
@@ -0,0 +1,89 @@
+//express init
+import express from "express";
+import {Sequelize} from "sequelize";
+
+var app = express();
+var port = 3000;
+
+//server
+app.listen(port, function(){
+    console.log('Server On!');
+});
+
+
+// view 파일이 위치한 폴더를 지정합니다.
+app.set('views', './views');
+app.set('js', './js');
+app.set('/', './');
+// view 엔진으로 ejs를 사용합니다.
+app.set('view engine', 'ejs');
+
+// '/' 경로로 GET 요청이 오면 index.ejs 파일을 렌더링하여 클라이언트에게 전송합니다.
+app.get('/', function(req, res) {
+
+    //Schedule.findAll order by date
+    Schedule.findAll({
+        order: [
+            ['date', 'ASC']
+        ]
+    }).then((schedules) => {
+        console.log(schedules);
+        res.render('index', {schedules: schedules});
+
+    });
+
+
+// res.render('index');
+});
+
+
+
+//static
+// app.use(express.static('public'));
+
+//init sqlite db and sequelize
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+//define model schedule
+const Schedule = db.define('schedule', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    date: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    time: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//init model
+db.sync()
+    .then(() => console.log('Database has been synced'))
+    .catch((err) => console.error('Unable to sync the database:', err));
+
+
+//insert data to schedule
+// Schedule.create({
+//     title: 'test',
+//     content: 'test',
+//     date: '2022-01-01',
+//     time: '12:00'
+// });
+
+
